1673672a79153d9d6b679fca52de610f8aa243f7
Gulena <a.p.sadovskiy@gmail.com>
2023-09-11 07:41:33 +0300

    2

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const bot = new TelegramApi(token, {polling: true});
 bot.on('message', msg=>{
 	const text = msg.text;
 	const chatId = msg.chat.id;
-	bot.sendMessage(chatId, `Вы написали мне:${text}`);
+	const first_name = msg.chat.first_name;
+	const last_name = msg.chat.last_name;
+	if(text==='/start') {
+		bot.sendMessage(chatId, `Добро пожаловать в тестовый бот`);
+	}
+	if(text==='/info') {
+		bot.sendMessage(chatId, `Вас зовут ${first_name}`);
+	}
+/* 	bot.sendMessage(chatId, `Вы написали мне:${text}`); */
 	/* console.log(msg); */
 })
